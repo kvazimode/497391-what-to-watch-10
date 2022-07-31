012bcba6165c1d04ae98e48b5b54d1675ba50334
kvazimode <kvazimode2@gmail.comn>
2022-08-01 00:25:09 +0300

    added routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import MainScreen from '../../pages/main/main';
+import SignIn from '../../pages/sign-in/sign-in';
+import MyList from '../../pages/my-list/my-list';
+import Film from '../../pages/film/film';
+import AddReview from '../../pages/add-review/add-review';
+import Player from '../../pages/player/player';
 import NotFound from '../../pages/not-found/not-found';
 
 type AppScreenProps = {
@@ -13,7 +19,7 @@ function App({cardCount, promoTitle, promoGenre, promoRelease}: AppScreenProps):
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={
+        <Route path={AppRoute.Main} element={
           <MainScreen
             promoTitle={promoTitle}
             promoGenre={promoGenre}
@@ -22,6 +28,11 @@ function App({cardCount, promoTitle, promoGenre, promoRelease}: AppScreenProps):
           />
         }
         />
+        <Route path={AppRoute.SignIn} element={<SignIn />} />
+        <Route path={AppRoute.MyList} element={<MyList />} />
+        <Route path={AppRoute.Film} element={<Film />} />
+        <Route path={AppRoute.AddReview} element={<AddReview />} />
+        <Route path={AppRoute.Player} element={<Player />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
